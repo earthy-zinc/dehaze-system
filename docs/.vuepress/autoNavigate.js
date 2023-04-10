5f361f6c0a3fe94a9b5a21c6eee27f20ca690961
earthy zinc <1066365803@qq.com>
2023-04-10 23:46:24 +0800

    添加了Nginx的知识

--- docs/.vuepress/autoNavigate.js
+++ docs/.vuepress/autoNavigate.js
@@ -12,23 +12,26 @@ function getNav(dirPath, arr){
         const fileDir = path.join(dirPath, filename);
         // 以同步方式获取当前文件信息
         const stats = fs.statSync(fileDir);
-    
+
         const isFile = stats.isFile();
         const isDir = stats.isDirectory();
-    
+
         if (isFile && (path.extname(fileDir) === '.md')) {
             // 处理了多余的绝对路径，第一个 replace 是替换掉rootPath之前的绝对路径
             // 第二个是所有满足\\的直接替换掉
             let nav = {
-                text: filename.replace('.md', ''), 
-                link: fileDir.replace(rootPath, '').replace(/\\/img, '/').replace('.md', '').replace('docs','')
+                text: filename.replace('.md', ''),
+                link: fileDir.replace(rootPath, '')
+                    .replace(/\\/img, '/')
+                    .replace('.md', '')
+                    .replace('docs','')
             };
             arr.push(nav);
         }
           // 如果是文件夹
         if (isDir) {
             let child = {
-                text: filename, 
+                text: filename,
                 items: []
             };
             arr.push(child);
