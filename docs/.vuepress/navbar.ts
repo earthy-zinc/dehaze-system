1d481b07f500773ac0b379cc9c0cc7f96db90400
earthy-zinc <1066365803@qq.com>
2023-10-28 11:41:16 +0800

    重大更新，进一步优化配置

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,71 +5,25 @@ export default navbar([
   {
     text: "前端开发",
     prefix: "/前端开发/",
-    children: [
-      {
-        text: "前端基础",
-        link: "前端基础/CSS.md"
-      },
-      {
-        text: "前端进阶",
-        link: "前端进阶/Babel.md"
-      },
-      {
-        text: "前端框架",
-        link: "前端框架/Vue2.md"
-      },
-    ],
+    children: ["前端基础","前端进阶","前端框架"],
   },
   {
     text: "后端开发",
     prefix: "/后端开发/",
-    children: [
-      {
-        text: "中间件",
-        link: "中间件/Netty.md",
-      },
-      {
-        text: "大数据",
-        link: "大数据/Flink 练习.md",
-      },
-      {
-        text: "开发知识",
-        link: "开发知识/CDN.md",
-      },
-      {
-        text: "操作系统",
-        link: "操作系统/Linux.md",
-      },
-      {
-        text: "数据库",
-        link: "数据库/MySQL.md",
-      },
-      {
-        text: "算法",
-        link: "算法/LeetCode.md",
-      },
-      {
-        text: "编程语言",
-        link: "编程语言/Java基础.mc",
-      },
-      {
-        text: "部署工具",
-        link: "部署工具/Docker.md",
-      },
-    ]
+    children: ["中间件","大数据","开发知识","操作系统","数据库","算法","编程语言","部署工具"]
   },
   {
     text: "学术课程",
-    link: "/学术课程/学术论文/LaTeX公式.md",
+    prefix: "/学术课程/",
+    children: ["学术论文","深度学习","研究生课程","研究生面试"]
   },
   {
     text: "工作效率",
-    link: "/工作效率/Git.md",
+    link: "/工作效率/",
   },
   {
     text: "项目文档",
-    link: "/项目文档/API网关/项目概要.md",
+    prefix: "/项目文档/",
+    children: ["API网关","中间件设计","土味商城","抽奖系统","沛信"],
   },
-
-
 ]);
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -26,71 +26,22 @@ export default hopeTheme({
 
   plugins: {
     autoCatalog: true,
-    // All features are enabled for demo, only preserve features you need here
     mdEnhance: {
       align: true,
       attrs: true,
       card: true,
-
-      // install chart.js before enabling it
-      // chart: true,
-
       codetabs: true,
       demo: true,
-
-      // install echarts before enabling it
-      // echarts: true,
-
       figure: true,
-
-      // install flowchart.ts before enabling it
-      // flowchart: true,
-
       gfm: true,
       imgLazyload: true,
       imgSize: true,
       include: true,
-
-      // install katex before enabling it
-      // katex: true,
-
-      // install mathjax-full before enabling it
-      // mathjax: true,
-
+      mathjax: true,
       mark: true,
-
-      // install mermaid before enabling it
-      // mermaid: true,
-
-      playground: {
-        presets: ["ts", "vue"],
-      },
-
-      // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
       sub: true,
       sup: true,
       tabs: true,
-      vPre: true,
-
-      // install @vue/repl before enabling it
-      // vuePlayground: true,
     },
   },
 });
