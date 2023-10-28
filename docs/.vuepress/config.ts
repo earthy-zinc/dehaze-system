6f024ed63ca35d58f70f8480e70e4e888b2c9503
earthy-zinc <1066365803@qq.com>
2023-10-28 11:18:05 +0800

    重大更新，将vuepress版本升级到2.0

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,20 @@
+import { defineUserConfig } from "vuepress";
+import {autoCatalogPlugin} from "vuepress-plugin-auto-catalog";
+// import fullTextSearchPlugin from "vuepress-plugin-full-text-search2";
+import theme from "./theme";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "土味锌的阅读笔记",
+  description: "全栈开发学习笔记",
+
+  theme,
+  plugins: [
+    autoCatalogPlugin({
+      //插件选项
+    }),
+    // fullTextSearchPlugin
+  ],
+});
---
+++ docs/.vuepress/sidebar.ts
@@ -0,0 +1,12 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/": [
+    "",
+  ],
+  "/前端开发": "structure",
+  "/后端开发": "structure",
+  "/学术课程": "structure",
+  "/工作效率": "structure",
+  "/项目文档": "structure",
+});
