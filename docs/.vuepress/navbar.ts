c06115316de847e8606d0bac2179d1854535b710
earthy-zinc <1066365803@qq.com>
2023-10-29 17:36:56 +0800

    小小修改

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -6,4 +6,62 @@ export default navbar([
     "/学术课程",
     "/工作效率",
     "/项目文档",
+    {
+        text: "项目展示",
+        children: [
+            {
+                text: "游戏开发",
+                children: [
+                    {
+                        text: "贪吃蛇",
+                        link: "/greedy_snake"
+                    }
+                ]
+            },
+            {
+                text: "学术应用",
+                children: [
+                    {
+                        text: "去雾系统",
+                        link: "/dehaze_tool"
+                    }
+                ]
+            },
+            {
+                text: "土味博客",
+                children: [
+                    {
+                        text: "小沛博客",
+                        link: "/blog"
+                    }
+                ]
+            },
+            {
+                text: "土味外卖",
+                children: [
+                    {
+                        text: "餐厅管理",
+                        link: "/deliver_shoper"
+                    },
+                    {
+                        text: "点单消费",
+                        link: "/deliver_customer"
+                    }
+                ]
+            },
+            {
+                text: "土味商城",
+                children: [
+                    {
+                        text: "卖家管理",
+                        link: "/shop_shoper"
+                    },
+                    {
+                        text: "下单购物",
+                        link: "/shop_customer"
+                    }
+                ]
+            }
+        ]
+    }
 ]);
