d4de665d752dff02d945577ce38d219c3759531f
土味锌 <1066365803@qq.com>
2024-01-14 22:17:49 +0800

    更新文件

--- my_test.py
+++ my_test.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import cv2
 import torch
 import torchvision
@@ -38,20 +40,43 @@ def img2tensor(imgs, bgr2rgb=True, float32=True):
         return _totensor(imgs, bgr2rgb, float32)
 
 
-with open("./options/RIDCP.yml", mode='r') as f:
+with open("./options/RIDCP-win.yml", mode='r') as f:
     opt = yaml.load(f, Loader=ordered_yaml()[0])
 
-hq_opt = opt['network_g'].copy()
-hq_opt['LQ_stage'] = False
 
-net_hq = build_network(hq_opt).to(DEVICE)
-net_hq.eval()
-with torch.no_grad():
-    for name, module in net_hq.named_modules():
-        # print(name)
-        if name == 'quantize_group.0.embedding':
-            print(module.weight)
-            print(module.weight.shape)
+net_g = build_network(opt['network_g']).to(DEVICE)
+print(net_g.state_dict().keys())
+
+print("-------load net-----------")
+load_net = torch.load("./pretrained_models/pretrained_RIDCP.pth")
+param_key = 'params'
+load_net = load_net[param_key]
+for key, param in load_net.items():
+    print(key)
+load_net.update({"quantizer.embedding.weight": load_net.pop("quantize_group.0.embedding.weight")})
+load_net.update({"before_quant.weight": load_net.pop("before_quant_group.0.weight")})
+load_net.update({"before_quant.bias": load_net.pop("before_quant_group.0.bias")})
+load_net.update({"after_quant.conv.weight": load_net.pop("after_quant_group.0.conv.weight")})
+load_net.update({"after_quant.conv.bias": load_net.pop("after_quant_group.0.conv.bias")})
+
+net_g.load_state_dict(load_net, strict=False)
+
+save_dict = {}
+state_dict = net_g.state_dict()
+for key, param in state_dict.items():
+    if key.startswith('module.'):  # remove unnecessary 'module.'
+        key = key[7:]
+    state_dict[key] = param.cpu()
+save_dict[param_key] = state_dict
+torch.save(save_dict, "./pretrained_models/pretrained_RIDCP_New.pth")
+
+# net_hq.eval()
+# with torch.no_grad():
+#     for name, module in net_hq.named_modules():
+#         # print(name)
+#         if name == 'quantize_group.0.embedding':
+#             print(module.weight)
+#             print(module.weight.shape)
 # net_hq.eval()
 # with torch.no_grad():
 #     img = cv2.imread("D://DeepLearning//dataset//RESIDE//OTS//haze//0025_0.8_0.2.jpg", cv2.IMREAD_UNCHANGED)
