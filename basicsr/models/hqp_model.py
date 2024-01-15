75d13d56a9e5c9b2f1db1d8dab2469c4357eab92
土味锌 <1066365803@qq.com>
2024-01-15 15:36:21 +0800

    继续存档

--- basicsr/models/hqp_model.py
+++ basicsr/models/hqp_model.py
@@ -16,7 +16,7 @@ import pyiqa
 
 
 @MODEL_REGISTRY.register()
-class RIDCPModel(BaseModel):
+class HQPModel(BaseModel):
     def __init__(self, opt):
         super().__init__(opt)
 
@@ -24,33 +24,11 @@ class RIDCPModel(BaseModel):
         self.net_g = build_network(opt['network_g'])
         self.net_g = self.model_to_device(self.net_g)
 
-        # load pre-trained HQ ckpt, frozen decoder and codebook
-        self.LQ_stage = self.opt['network_g'].get('LQ_stage', False)
-        if self.LQ_stage:
-            load_path = self.opt['path'].get('pretrain_network_hq', None)
-            assert load_path is not None, 'Need to specify hq prior model path in LQ stage'
-
-            hq_opt = self.opt['network_g'].copy()
-            hq_opt['LQ_stage'] = False
-            self.net_hq = build_network(hq_opt)
-            self.net_hq = self.model_to_device(self.net_hq)
-            self.load_network(self.net_hq, load_path, self.opt['path']['strict_load'])
-
-            self.load_network(self.net_g, load_path, False)
-            frozen_module_keywords = self.opt['network_g'].get('frozen_module_keywords', None)
-            if frozen_module_keywords is not None:
-                for name, module in self.net_g.named_modules():
-                    for fkw in frozen_module_keywords:
-                        if fkw in name:
-                            for p in module.parameters():
-                                p.requires_grad = False
-                            break
-
         # load pretrained models
         load_path = self.opt['path'].get('pretrain_network_g', None)
         logger = get_root_logger()
         if load_path is not None:
-            logger.info(f'从 {load_path} 中加载去雾网络（network generator）')
+            logger.info(f'从 {load_path} 中加载生成网络（network generator）')
             self.load_network(self.net_g, load_path, self.opt['path']['strict_load'])
 
         # define metric functions
@@ -130,12 +108,7 @@ class RIDCPModel(BaseModel):
         self.optimizers.append(self.optimizer_d)
 
     def feed_data(self, data):
-        if 'lq' in data:
-            self.lq = data['lq'].to(self.device)
-        else:
-            self.lq = None
-        if 'gt' in data:
-            self.gt = data['gt'].to(self.device)
+        self.gt = data['gt'].to(self.device)
 
     def optimize_parameters(self, current_iter):
         train_opt = self.opt['train']
@@ -143,21 +116,10 @@ class RIDCPModel(BaseModel):
         for p in self.net_d.parameters():
             p.requires_grad = False
         self.optimizer_g.zero_grad()
-
-        if self.LQ_stage:
-            with torch.no_grad():
-                self.gt_rec, _, _, _, _, quant_gt, gt_indices = self.net_hq(self.gt)
-            self.lq.requires_grad = True
-            self.output, self.output_residual, l_codebook, l_semantic, quant_g, _, _ = self.net_g(self.lq, gt_indices)
-        else:
-            self.output, self.output_residual, l_codebook, l_semantic, quant_g, _, _ = self.net_g(self.gt)
+        self.output, l_codebook, l_semantic, feat_to_quant, quant_gt, gt_indices = self.net_g(self.gt)
 
         l_g_total = torch.zeros((1,)).to(self.device)
         loss_dict = OrderedDict()
-
-        if self.output_residual is None:
-            self.output_residual = self.output
-            quant_gt = quant_g
 
         # ===================================================
         # codebook loss
@@ -175,13 +137,13 @@ class RIDCPModel(BaseModel):
 
         # pixel loss
         if self.cri_pix:
-            l_pix = self.cri_pix(self.output_residual, self.gt)
+            l_pix = self.cri_pix(self.output, self.gt)
             l_g_total += l_pix
             loss_dict['l_pix'] = l_pix
 
         # perceptual loss
         if self.cri_perceptual:
-            l_percep, l_style = self.cri_perceptual(self.output_residual, self.gt)
+            l_percep, l_style = self.cri_perceptual(self.output, self.gt)
             if l_percep is not None:
                 l_g_total += l_percep.mean()
                 loss_dict['l_percep'] = l_percep.mean()
@@ -191,15 +153,12 @@ class RIDCPModel(BaseModel):
 
         # gan loss
         if self.use_dis and current_iter > train_opt['net_d_init_iters']:
-            fake_g_pred = self.net_d(quant_g)
+            fake_g_pred = self.net_d(quant_gt)
             l_g_gan = self.cri_gan(fake_g_pred, True, is_disc=False)
             l_g_total += l_g_gan
             loss_dict['l_g_gan'] = l_g_gan
 
-        # print(l_g_total.requires_grad)
-        # if l_g_total.requires_grad:
         l_g_total.mean().backward(retain_graph=True)
-
 
         # optimize net_d
         self.fixed_disc = self.opt['train'].get('fixed_disc', False)
@@ -213,29 +172,22 @@ class RIDCPModel(BaseModel):
             loss_dict['l_d_real'] = l_d_real
             loss_dict['out_d_real'] = torch.mean(real_d_pred.detach())
             l_d_real.backward()
-            # fake
-            fake_d_pred = self.net_d(quant_g.detach())
-            l_d_fake = self.cri_gan(fake_d_pred, False, is_disc=True)
-            loss_dict['l_d_fake'] = l_d_fake
-            loss_dict['out_d_fake'] = torch.mean(fake_d_pred.detach())
-            l_d_fake.backward()
+            self.optimizer_g.step()
             self.optimizer_d.step()
-        self.optimizer_g.step()
+        else:
+            self.optimizer_g.step()
         self.log_dict = self.reduce_loss_dict(loss_dict)
 
     def test(self):
         self.net_g.eval()
         net_g = self.get_bare_model(self.net_g)
         min_size = 8000 * 8000  # use smaller min_size with limited GPU memory
-        if self.lq is not None:
-            inputs = self.lq
-        else:
-            inputs = self.gt
-        _, _, h, w = inputs.shape
+        lq_input = self.lq
+        _, _, h, w = lq_input.shape
         if h * w < min_size:
-            self.output, _ = net_g.test(inputs)
+            self.output, _ = net_g.test(lq_input)
         else:
-            self.output = net_g.test_tile(inputs)
+            self.output = net_g.test_tile(lq_input)
         self.net_g.train()
 
     def dist_validation(self, dataloader, current_iter, tb_logger, save_img, save_as_dir=None):
@@ -266,8 +218,7 @@ class RIDCPModel(BaseModel):
             self.key_metric = self.opt['val'].get('key_metric')
 
         for idx, val_data in enumerate(dataloader):
-
-            img_name = osp.splitext(osp.basename(val_data['gt_path'][0]))[0]
+            img_name = osp.splitext(osp.basename(val_data['lq_path'][0]))[0]
             self.feed_data(val_data)
             self.test()
 
@@ -359,30 +310,20 @@ class RIDCPModel(BaseModel):
 
     def vis_single_code(self, up_factor=2):
         net_g = self.get_bare_model(self.net_g)
-        codenum = self.opt['network_g']["codebook_emb_num"]
+        codebook_emb_num = self.opt['network_g']['codebook_emb_num']
         with torch.no_grad():
-            code_idx = torch.arange(codenum).reshape(codenum, 1, 1, 1)
+            code_idx = torch.arange(codebook_emb_num).reshape(codebook_emb_num, 1, 1, 1)
             code_idx = code_idx.repeat(1, 1, up_factor, up_factor)
             output_img = net_g.decode_indices(code_idx)
             output_img = tvu.make_grid(output_img, nrow=32)
-
         return output_img.unsqueeze(0)
 
     def get_current_visuals(self):
         vis_samples = 16
         out_dict = OrderedDict()
-        if self.lq != None:
-            out_dict['lq'] = self.lq.detach().cpu()[:vis_samples]
-        if self.output != None:
-            out_dict['result_codebook'] = self.output.detach().cpu()[:vis_samples]
-        if self.output_residual != None:
-            out_dict['result_residual'] = self.output_residual.detach().cpu()[:vis_samples]
-        if not self.LQ_stage:
-            out_dict['codebook'] = self.vis_single_code()
-        if hasattr(self, 'gt_rec'):
-            out_dict['gt_rec'] = self.gt_rec.detach().cpu()[:vis_samples]
-        if hasattr(self, 'gt'):
-            out_dict['gt'] = self.gt.detach().cpu()[:vis_samples]
+        out_dict['gt_rec'] = self.output.detach().cpu()[:vis_samples]
+        out_dict['codebook'] = self.vis_single_code()
+        out_dict['gt'] = self.gt.detach().cpu()[:vis_samples]
         return out_dict
 
     def save(self, epoch, current_iter):
