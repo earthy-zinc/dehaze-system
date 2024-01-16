cbb15c267c95b236e5d81cf09eae396c72135a53
土味锌 <1066365803@qq.com>
2024-01-16 14:08:31 +0800

    继续存档

---
+++ basicsr/archs/itb_arch.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn
+
+from basicsr.archs.module.rcan import RCAN
+from basicsr.archs.ridcp_arch import RIDCP
+from basicsr.utils.registry import ARCH_REGISTRY
+
+
+@ARCH_REGISTRY.register()
+class FusionRefine(nn.Module):
+    def __init__(self, opt, **kwargs):
+        super(FusionRefine, self).__init__()
+        print(opt)
+        print(kwargs)
+        # first branch
+        self.feature_extract = RIDCP(**opt)
+        # second branch
+        self.pre_trained_rcan = RCAN()
+        self.tail = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(35, 3, kernel_size=7, padding=0),
+            nn.Tanh()
+        )
+
+    def forward(self, inputs, gt_indices=None):
+        out_img, out_img_residual, codebook_loss, semantic_loss, feat_to_quant, z_quant, indices = self.feature_extract(inputs, gt_indices)
+        rcan_out = self.pre_trained_rcan(inputs)
+        x = torch.cat([out_img_residual, rcan_out], 1)
+        # print("out_img_residual.shape")
+        # print(rcan_out.shape)
+        # print(out_img_residual.shape)
+        # print(x.shape)
+        feat_hazy = self.tail(x)
+        return out_img, feat_hazy, codebook_loss, semantic_loss, feat_to_quant, z_quant, indices
+
+    @torch.no_grad()
+    def test_tile(self, inputs):
+        x = self.feature_extract.test_tile(inputs)
+        y = self.pre_trained_rcan(inputs)
+        out = torch.cat([x, y], 1)
+        return self.tail(out)
+
+    @torch.no_grad()
+    def test(self, inputs):
+        x, index = self.feature_extract.test(inputs)
+        y = self.pre_trained_rcan(inputs)
+        out = torch.cat([x, y], 1)
+        return self.tail(out), index
