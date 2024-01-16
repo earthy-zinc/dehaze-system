12a04aaaf702943126890f4ef265c26d11cc738d
土味锌 <1066365803@qq.com>
2024-01-16 17:35:00 +0800

    继续存档

--- basicsr/archs/ridcp/decoder.py
+++ basicsr/archs/ridcp/decoder.py
@@ -8,19 +8,15 @@ class DecoderBlock(nn.Module):
 
     def __init__(self, in_channel, out_channel, norm_type='gn', act_type='leakyrelu'):
         super().__init__()
-
-        self.block = []
-        self.block += [
+        self.block = nn.Sequential(
             nn.Upsample(scale_factor=2),
             nn.Conv2d(in_channel, out_channel, 3, stride=1, padding=1),
             ResBlock(out_channel, out_channel, norm_type, act_type),
             ResBlock(out_channel, out_channel, norm_type, act_type),
-        ]
+        )
 
-        self.block = nn.Sequential(*self.block)
-
-    def forward(self, input):
-        return self.block(input)
+    def forward(self, inputs):
+        return self.block(inputs)
 
 
 class MultiScaleDecoder(nn.Module):
@@ -54,6 +50,77 @@ class MultiScaleDecoder(nn.Module):
             res = res * 2
 
     def forward(self, x, code_decoder_output):
+        for idx, m in enumerate(self.upsampler):
+            with torch.backends.cudnn.flags(enabled=False):
+                if not self.only_residual:
+                    x = m(x)
+                    if self.use_warp:
+                        x_vq = self.warp[idx](code_decoder_output[idx], x)
+                        x = x + x_vq * (x.mean() / x_vq.mean())
+                    else:
+                        x = x + code_decoder_output[idx]
+                else:
+                    x = m(x)
+        return x
+
+
+class VQDecoder(nn.Module):
+    def __init__(self,
+                 max_depth,
+                 input_res=256,
+                 channel_query_dict=None,
+                 norm_type='gn',
+                 act_type='leakyrelu',):
+        super(VQDecoder, self).__init__()
+
+        self.decoder_group = nn.ModuleList()
+        for i in range(max_depth):
+            res = input_res // 2 ** max_depth * 2 ** i
+            in_ch, out_ch = channel_query_dict[res], channel_query_dict[res * 2]
+            self.decoder_group.append(
+                DecoderBlock(in_ch, out_ch, norm_type, act_type)
+            )
+
+    def forward(self, x):
+        for idx, m in enumerate(self.decoder_group):
+            x = self.decoder_group[idx](x)
+        return x
+
+
+class RIDCPDecoder(nn.Module):
+    def __init__(self,
+                 in_channel,
+                 max_depth,
+                 input_res=256,
+                 channel_query_dict=None,
+                 norm_type='gn',
+                 act_type='leakyrelu',
+                 only_residual=False,
+                 use_warp=True
+                 ):
+        super().__init__()
+        self.only_residual = only_residual
+        self.use_warp = use_warp
+        self.upsampler = nn.ModuleList()
+        self.warp = nn.ModuleList()
+        res = input_res // (2 ** max_depth)
+        for i in range(max_depth):
+            in_channel, out_channel = channel_query_dict[res], channel_query_dict[res * 2]
+            self.upsampler.append(
+                nn.Sequential(
+                    nn.Upsample(scale_factor=2),
+                    nn.Conv2d(in_channel, out_channel, 3, stride=1, padding=1),
+                    ResBlock(out_channel, out_channel, norm_type, act_type),
+                    ResBlock(out_channel, out_channel, norm_type, act_type),
+                )
+            )
+            self.warp.append(WarpBlock(out_channel))
+            res = res * 2
+
+    def forward(self, x, code_decoder_output):
+        # in x (batch_size, 256, w, h)
+        # in code_decoder_output list length 2 shape (batch_size, 256, w, h)
+        # out channel 256 -> 128 -> 64
         for idx, m in enumerate(self.upsampler):
             with torch.backends.cudnn.flags(enabled=False):
                 if not self.only_residual:
