e61a9f5a2745ec887e910c4aec86b345c25cbcd4
土味锌 <1066365803@qq.com>
2024-03-09 16:43:10 +0800

    修改了消融实验配置文件，更新了数据集创建过程

--- basicsr/models/itb_new_model.py
+++ basicsr/models/itb_new_model.py
@@ -292,7 +292,7 @@ class ITBNewModel(BaseModel):
         self.nondist_validation(dataloader, current_iter, tb_logger, save_img, save_as_dir)
 
     def nondist_validation(self, dataloader, current_iter, tb_logger,
-                           save_img, save_as_dir):
+                           save_img, save_as_dir, update_net=True):
         dataset_name = dataloader.dataset.opt['name']
         with_metrics = self.opt['val'].get('metrics') is not None
         if with_metrics:
@@ -356,34 +356,39 @@ class ITBNewModel(BaseModel):
             # calculate average metric
             for metric in self.metric_results.keys():
                 self.metric_results[metric] /= (idx + 1)
+            if update_net:
+                if self.key_metric is not None:
+                    # If the best metric is updated, update and save best model
+                    to_update = self._update_best_metric_result(dataset_name, self.key_metric,
+                                                                self.metric_results[self.key_metric], current_iter)
 
-            if self.key_metric is not None:
-                # If the best metric is updated, update and save best model
-                to_update = self._update_best_metric_result(dataset_name, self.key_metric,
-                                                            self.metric_results[self.key_metric], current_iter)
-
-                if to_update:
+                    if to_update:
+                        for name, opt_ in self.opt['val']['metrics'].items():
+                            self._update_metric_result(dataset_name, name, self.metric_results[name], current_iter)
+                        self.copy_model(self.net_g, self.net_g_best)
+                        self.copy_model(self.net_d, self.net_d_best)
+                        self.save_network(self.net_g, 'net_g_best', '')
+                        self.save_network(self.net_d, 'net_d_best', '')
+                else:
+                    # update each metric separately
+                    updated = []
                     for name, opt_ in self.opt['val']['metrics'].items():
-                        self._update_metric_result(dataset_name, name, self.metric_results[name], current_iter)
-                    self.copy_model(self.net_g, self.net_g_best)
-                    self.copy_model(self.net_d, self.net_d_best)
-                    self.save_network(self.net_g, 'net_g_best', '')
-                    self.save_network(self.net_d, 'net_d_best', '')
-            else:
-                # update each metric separately
-                updated = []
-                for name, opt_ in self.opt['val']['metrics'].items():
-                    tmp_updated = self._update_best_metric_result(dataset_name, name, self.metric_results[name],
-                                                                  current_iter)
-                    updated.append(tmp_updated)
-                # save best model if any metric is updated
-                if sum(updated):
-                    self.copy_model(self.net_g, self.net_g_best)
-                    self.copy_model(self.net_d, self.net_d_best)
-                    self.save_network(self.net_g, 'net_g_best', '')
-                    self.save_network(self.net_d, 'net_d_best', '')
+                        tmp_updated = self._update_best_metric_result(dataset_name, name, self.metric_results[name],
+                                                                      current_iter)
+                        updated.append(tmp_updated)
+                    # save best model if any metric is updated
+                    if sum(updated):
+                        self.copy_model(self.net_g, self.net_g_best)
+                        self.copy_model(self.net_d, self.net_d_best)
+                        self.save_network(self.net_g, 'net_g_best', '')
+                        self.save_network(self.net_d, 'net_d_best', '')
 
             self._log_validation_metric_values(current_iter, dataset_name, tb_logger)
+
+    def nondist_test(self, net, dataloader, current_iter, tb_logger, save_img):
+        self.net_g = net
+        self.nondist_validation(dataloader, current_iter, tb_logger,
+                                save_img, None, False)
 
     def _log_validation_metric_values(self, current_iter, dataset_name, tb_logger):
         log_str = f'验证集 {dataset_name}\n'
