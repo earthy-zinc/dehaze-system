00e637891e042e4415253476bfe0c664832b3b3e
土味锌 <1066365803@qq.com>
2024-05-06 22:11:53 +0800

    refactor: 重构部分代码，组织页面

--- src/settings.ts
+++ src/settings.ts
@@ -6,17 +6,17 @@ import { LanguageEnum } from "./enums/LanguageEnum";
 const { pkg } = __APP_INFO__;
 
 const defaultSettings: AppSettings = {
-  title: pkg.name,
+  title: "图像去雾系统",
   version: pkg.version,
   showSettings: true,
-  tagsView: true,
-  fixedHeader: true,
+  tagsView: false,
+  fixedHeader: false,
   sidebarLogo: true,
-  layout: LayoutEnum.LEFT,
+  layout: LayoutEnum.TOP,
   theme: ThemeEnum.LIGHT,
   size: SizeEnum.DEFAULT,
   language: LanguageEnum.ZH_CN,
-  themeColor: "#409EFF",
+  themeColor: "#117B07",
   watermarkEnabled: false,
   watermarkContent: pkg.name,
 };
