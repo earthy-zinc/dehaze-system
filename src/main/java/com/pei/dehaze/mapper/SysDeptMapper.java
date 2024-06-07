13a52bcbf32aa1325d82a908049695b5b9a1fe2e
earthy-zinc <1066365803@qq.com>
2024-06-07 22:51:24 +0800

    初始化项目

---
+++ src/main/java/com/pei/dehaze/mapper/SysDeptMapper.java
@@ -0,0 +1,20 @@
+package com.pei.dehaze.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.pei.dehaze.model.entity.SysDept;
+import com.pei.dehaze.plugin.mybatis.annotation.DataPermission;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+
+@Mapper
+public interface SysDeptMapper extends BaseMapper<SysDept> {
+
+    @DataPermission(deptIdColumnName = "id")
+    @Override
+    List<SysDept> selectList(@Param(Constants.WRAPPER) Wrapper<SysDept> queryWrapper);
+}
