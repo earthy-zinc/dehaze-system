acb4181f644937dd4242b4051fe687dba538380e
earthy-zinc <1066365803@qq.com>
2024-06-08 00:16:20 +0800

    feat: :sparkles: 增加懒加载和瀑布流相关组件

---
+++ src/components/Magnifier/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineOptions({
+  name: "Magnifier",
+});
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: "",
+  },
+  highSrc: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: Number,
+    default: 0,
+  },
+  height: {
+    type: Number,
+    default: 0,
+  },
+  scale: {
+    type: Number,
+    default: 1.5,
+  },
+});
+</script>
+
+<template>
+  <div>test</div>
+</template>
+
+<style lang="scss" scoped></style>
