315f1ac0f25c49290a6aa7ac39250890e53e8df7
haonan66 <731331417@qq.com>
2024-06-21 15:26:54 +0800

    feat: :sparkles:新增数据集图片纵向无感滚动

--- src/components/Waterfall/index.vue
+++ src/components/Waterfall/index.vue
@@ -2,6 +2,7 @@
 import { getValue } from "@/utils";
 import Lazy from "../LazyImg/lazy";
 import { LazyType } from "../LazyImg/types";
+import LazyImg from "../LazyImg/index.vue";
 import { useCalculateCols, useLayout } from "./waterfall";
 import { ViewCard } from "./types";
 
@@ -95,6 +96,10 @@ const props = defineProps({
     type: String,
     default: "center",
   },
+  speed: {
+    type: Number,
+    default: 1,
+  },
 });
 
 const emit = defineEmits(["afterRender"]);
@@ -111,7 +116,7 @@ const { waterfallWrapper, wrapperWidth, colWidth, cols, offsetX } =
   useCalculateCols(props);
 
 // 容器高度，块定位
-const { wrapperHeight, layoutHandle } = useLayout(
+const { wrapperHeight, itemHeight, layoutHandle } = useLayout(
   props,
   colWidth,
   cols,
@@ -138,10 +143,14 @@ watch(
 // 尺寸宽度变化防抖触发
 const sizeChangeTime = ref(0);
 
-// watchDebounced(colWidth, () => {
-//   layoutHandle()
-//   sizeChangeTime.value += 1
-// }, { debounce: props.delay })
+watchDebounced(
+  colWidth,
+  () => {
+    layoutHandle();
+    sizeChangeTime.value += 1;
+  },
+  { debounce: props.delay }
+);
 
 provide("sizeChangeTime", sizeChangeTime);
 
@@ -157,26 +166,82 @@ const getRenderURL = (item: ViewCard): string => {
 const getKey = (item: ViewCard, index: number): string => {
   return item[props.rowKey] || index;
 };
+
+// 定时器
+let timer: number;
+
+// 垂直方向移动距离
+const translateY = ref(0);
+
+const scrollAmount = computed(() => {
+  return props.speed;
+});
+
+const allWrapper = ref();
+
+// 滚动函数
+const scroll = (): void => {
+  translateY.value -= scrollAmount.value;
+  // 当图片完全滚动出视口时，将其重新放置在顶部实现无缝滚动
+  if (props.speed < 0) {
+    if (translateY.value >= 0) {
+      translateY.value = -wrapperHeight.value + 700;
+    }
+  } else {
+    if (translateY.value <= -wrapperHeight.value + 700) {
+      translateY.value = 0;
+    }
+  }
+};
+
+// 鼠标移入图片区域暂停滚动
+const stop = () => {
+  timer && clearInterval(timer);
+};
+// 鼠标移出图片区域开始滚动
+const start = () => {
+  timer = +setInterval(scroll, 1000 / 120);
+};
+
+onMounted(() => {
+  timer = +setInterval(scroll, 1000 / 120);
+});
+
+onUnmounted(() => {
+  timer && clearInterval(timer);
+});
 </script>
 
 <template>
-  <div
-    ref="waterfallWrapper"
-    class="waterfall-list"
-    :style="{ height: `${wrapperHeight}px`, backgroundColor }"
-  >
+  <div class="all-wrapper" ref="allWrapper" style="height: 700px">
     <div
-      v-for="(item, index) in list"
-      :key="getKey(item, index)"
-      class="waterfall-item"
+      ref="waterfallWrapper"
+      class="waterfall-list"
+      :style="{
+        height: `${wrapperHeight}px`,
+        backgroundColor,
+        transform: `translateY(${translateY}px)`,
+      }"
+      @mouseenter="stop"
+      @mouseleave="start"
     >
-      <div class="waterfall-card">
-        <slot
-          name="item"
-          :item="item"
-          :index="index"
-          :url="getRenderURL(item)"
-        ></slot>
+      <div
+        v-for="(item, index) in list"
+        :key="getKey(item, index)"
+        class="waterfall-item"
+      >
+        <div class="waterfall-card">
+          <LazyImg :url="getRenderURL(item)" />
+        </div>
+      </div>
+      <div
+        v-for="(item, index) in list"
+        :key="getKey(item, index)"
+        class="waterfall-item"
+      >
+        <div class="waterfall-card">
+          <LazyImg :url="getRenderURL(item)" />
+        </div>
       </div>
     </div>
   </div>
@@ -193,10 +258,15 @@ const getKey = (item: ViewCard, index: number): string => {
   position: absolute;
   top: 0;
   left: 0;
+  cursor: pointer;
   visibility: hidden;
 
   /* 初始位置设置到屏幕以外，避免懒加载失败 */
   transform: translate3d(0, 3000px, 0);
+}
+
+.waterfall-item:nth-child(2) {
+  transition-delay: calc(1000 / 120 / 2) s;
 }
 
 /* 初始的入场效果 */
