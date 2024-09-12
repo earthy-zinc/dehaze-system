0eedb61a89f72371b2816c65d4a2f423db3a0ffd
haonan66 <731331417@qq.com>
2024-09-12 19:51:05 +0800

    feat: :sparkles:新增模型管理界面

--- src/api/algorithm/model.ts
+++ src/api/algorithm/model.ts
@@ -1,5 +1,5 @@
 /**
- * 数据集查询参数类型
+ * 模型查询参数类型
  */
 export interface AlgorithmQuery {
   keywords?: string;
@@ -7,34 +7,39 @@ export interface AlgorithmQuery {
 
 export interface Algorithm {
   id: number;
+  parentId: number;
   name: string;
   type: string;
   description: string;
-  importPath: string;
-  children: Model[];
-}
-
-interface ModelWithPath {
-  id: number;
-  name: string;
-  type: string;
-  description: string;
-  path: string;
-  /**
-   * 状态(1:启用；0:禁用)
-   */
+  path?: string;
+  importPath?: string;
   status?: number;
-  size: number;
-  children?: never; // 确保 children 不被定义
+  size?: string;
+  children?: Algorithm[];
 }
 
-interface ModelWithChildren {
-  id: number;
-  name: string;
-  type: string;
-  description: string;
-  path?: never; // 确保 path 不被定义
-  children: ModelWithChildren[]; // 注意这里的类型应该是自身接口的数组，形成递归
-}
+// interface ModelWithPath {
+//   id: number;
+//   name: string;
+//   type: string;
+//   description: string;
+//   path: string;
+//   /**
+//    * 状态(1:启用；0:禁用)
+//    */
+//   status: number;
+//   size: string;
+//   children?: never; // 确保 children 不被定义
+// }
 
-export type Model = ModelWithPath | ModelWithChildren;
+// interface ModelWithChildren {
+//   id: number;
+//   parentId: number;
+//   name: string;
+//   type: string;
+//   description: string;
+//   path?: never; // 确保 path 不被定义
+//   children: ModelWithChildren[]; // 注意这里的类型应该是自身接口的数组，形成递归
+// }
+
+// export type Model = ModelWithPath | ModelWithChildren;
