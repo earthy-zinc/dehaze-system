a432dfeb29df7ea47067fff081c2e16b239207f2
土味锌 <1066365803@qq.com>
2024-11-15 13:50:44 +0800

    feat: 重构文件上传与数据集管理模块

---
+++ src/main/java/com/pei/dehaze/service/impl/SysDatasetFileServiceImpl.java
@@ -0,0 +1,18 @@
+package com.pei.dehaze.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pei.dehaze.mapper.SysDatasetFileMapper;
+import com.pei.dehaze.model.entity.SysDatasetFile;
+import com.pei.dehaze.service.SysDatasetFileService;
+
+public class SysDatasetFileServiceImpl extends ServiceImpl<SysDatasetFileMapper, SysDatasetFile>
+        implements SysDatasetFileService  {
+    @Override
+    public Long getMaxImageItemId() {
+        QueryWrapper<SysDatasetFile> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("MAX(image_item_id) as maxImageItemId");
+        SysDatasetFile result = this.getOne(queryWrapper);
+        return result != null ? result.getImageItemId() : 0L;
+    }
+}
