71f257670ad8898216c92d7f0cc33ed479461a16
土味锌 <1066365803@qq.com>
2024-12-06 00:12:44 +0800

    优化文件处理逻辑及移除冗余依赖

--- src/main/java/com/pei/dehaze/service/impl/SysPredLogServiceImpl.java
+++ src/main/java/com/pei/dehaze/service/impl/SysPredLogServiceImpl.java
@@ -1,11 +1,11 @@
 package com.pei.dehaze.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import com.pei.dehaze.mapper.SysPredLogMapper;
 import com.pei.dehaze.model.entity.SysPredLog;
 import com.pei.dehaze.service.SysPredLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/pei/dehaze/service/impl/SysWpxFileServiceImpl.java
+++ src/main/java/com/pei/dehaze/service/impl/SysWpxFileServiceImpl.java
@@ -1,11 +1,11 @@
 package com.pei.dehaze.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import com.pei.dehaze.mapper.SysWpxFileMapper;
 import com.pei.dehaze.model.entity.SysWpxFile;
 import com.pei.dehaze.service.SysWpxFileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
