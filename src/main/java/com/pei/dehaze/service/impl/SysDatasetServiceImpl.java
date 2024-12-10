2c0b0c5a040f4388ef5f9b71c07cd43ff460e9ac
土味锌 <1066365803@qq.com>
2024-12-10 16:06:17 +0800

    refactor(dataset): 调整数据集初始化策略并优化相关代码
    - 移除了 SysDatasetServiceImpl 中未使用的 import 语句- 在 InitFile 类中删除了冗余的注释和不必要的条件判断
    - 修改了 application-dev.yml 中的数据集初始化配置，将其设置为 false

--- src/main/java/com/pei/dehaze/service/impl/SysDatasetServiceImpl.java
+++ src/main/java/com/pei/dehaze/service/impl/SysDatasetServiceImpl.java
@@ -20,7 +20,6 @@ import com.pei.dehaze.service.SysDatasetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
