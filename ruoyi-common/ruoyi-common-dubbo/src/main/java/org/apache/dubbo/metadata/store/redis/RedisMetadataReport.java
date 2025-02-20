6fa6caa1afc0dbbd231c12c22daa6c4d88008593
疯狂的狮子Li <15040126243@163.com>
2025-02-20 14:35:22 +0800

    update dubbo 3.2.14 => 3.3.3 支持虚拟线程

--- ruoyi-common/ruoyi-common-dubbo/src/main/java/org/apache/dubbo/metadata/store/redis/RedisMetadataReport.java
+++ ruoyi-common/ruoyi-common-dubbo/src/main/java/org/apache/dubbo/metadata/store/redis/RedisMetadataReport.java
@@ -48,6 +48,8 @@ import static org.apache.dubbo.metadata.report.support.Constants.DEFAULT_METADAT
  */
 public class RedisMetadataReport extends AbstractMetadataReport {
 
+    private static final int ONE_DAY_IN_MILLISECONDS = 86400000;
+
     private static final String REDIS_DATABASE_KEY = "database";
     private static final ErrorTypeAwareLogger logger = LoggerFactory.getErrorTypeAwareLogger(RedisMetadataReport.class);
 
