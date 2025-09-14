abbdd77d21c5825791dac6981584a01db8ceb845
土味锌 <1066365803@qq.com>
2025-09-14 22:14:22 +0800

    feat: 优化多对多关系的外键和引用，更新用户认证信息查询逻辑，修复测试用例中的断言

--- model/sys_role.go
+++ model/sys_role.go
@@ -9,6 +9,6 @@ type SysRole struct {
 	Status    int8      `gorm:"default:1;comment:角色状态(1-正常；0-停用)" json:"status"`
 	DataScope int8      `gorm:"comment:数据权限(0-所有数据；1-部门及子部门数据；2-本部门数据；3-本人数据)" json:"dataScope"`
 	Deleted   int8      `gorm:"not null;default:0;comment:逻辑删除标识(0-未删除；1-已删除)" json:"deleted"`
-	Users     []SysUser `gorm:"many2many:sys_user_role;"`
-	Menus     []SysMenu `gorm:"many2many:sys_role_menu;"`
+	Users     []SysUser `gorm:"many2many:sys_user_role;joinForeignKey:role_id;joinReferences:user_id"`
+	Menus     []SysMenu `gorm:"many2many:sys_role_menu;joinForeignKey:role_id;joinReferences:menu_id"`
 }
