b77af117899138adfc3e71079451ac46633fc38f
earthy-zinc <1066365803@qq.com>
2025-10-04 12:09:14 +0800

    Remove dehaze-doc subtree for re-add with master branch

--- dehaze-doc/README.md
+++
@@ -1,7 +0,0 @@
-# 土味锌的阅读笔记
-
-这是一个全栈开发学习笔记，运行以下命令启动项目
-
-```shell
-pnpm install && pnpm run docs:dev
-```
--- dehaze-doc/docs/.vuepress/sidebar.ts
+++
@@ -1,9 +0,0 @@
-import { sidebar } from "vuepress-theme-hope";
-
-export default sidebar({
-  "/前端开发": "structure",
-  "/后端开发": "structure",
-  "/学术课程": "structure",
-  "/通用工具": "structure",
-  "/项目文档": "structure",
-});
