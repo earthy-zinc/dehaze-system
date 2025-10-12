a6123d3fb747e8abd64bf1963aa6aed7774f91d3
土味锌 <1066365803@qq.com>
2025-10-12 15:20:25 +0800

    feat(android): 添加相机和存储权限并更新依赖
    - 添加 CAMERA、RECORD_AUDIO、WRITE_EXTERNAL_STORAGE 和 READ_EXTERNAL_STORAGE 权限
    - 添加网络状态和 WiFi 状态权限
    - 添加硬件相机功能声明
    - 启用 Gradle 并行构建、缓存和守护进程
    - 启用 React Native 新架构
    - 移除 react-native-camera 和 react-native-push-notification 依赖
    - 添加 react-native-vision-camera 依赖
    - 更新 App 组件以支持手势处理
    - 更新 tsconfig 包含 index.js 文件

--- dehaze-react-native/src/App.tsx
+++ dehaze-react-native/src/App.tsx
@@ -1,15 +1,17 @@
 import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './navigation/NavigationContainer';
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppNavigator />
-    </SafeAreaProvider>
+    <GestureHandlerRootView >
+      <SafeAreaProvider>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <AppNavigator />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
 
